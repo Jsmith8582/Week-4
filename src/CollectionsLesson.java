41ee1356bef551da2a2da9493fd420d6bd6e2e66
Josh Smith <Jsmith8582@gmail.com>
2020-04-21 15:33:49 -0600

    Stringbuilding and Lists

---
+++ src/CollectionsLesson.java
@@ -0,0 +1,105 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class CollectionsLesson {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Lists allow duplicates
+		//Keeps elements by index
+		//Allows null values
+		//common implementations: ArrayList, LinkedList, Vector
+		
+		
+		
+		
+		List<String> students = new ArrayList<String>();
+		students.add("Rob");
+		students.add("Rob");
+		students.add("Sam");
+		students.add(null);
+		
+		
+		for (String student : students) {
+			System.out.println(student);
+		}
+		
+		System.out.println(students.get(0));
+		System.out.println(students.get(2));
+		
+		/*
+		 * Set
+		 * 
+		 * No Duplicate
+		 * Unordered, Wont return in the same position as typed in.
+		 * Common Implementations: HashSet, LinkedHashSet, TreeSet.
+		 */
+		
+		Set<String> states = new HashSet<String>();
+		states.add("Alabama");
+		states.add("Alaska");
+		states.add("Arkansas");
+		states.add("Arizona");
+		states.add(null);
+		
+		System.out.println(states.contains("Alabama"));
+		if (states.contains("Alabama")) {
+		System.out.println(states.remove("Alabama"));
+		}
+		
+		for (String state : states) {
+			System.out.println(state);
+		}
+		
+		/*
+		 * Map
+		 * Map<K,V> K is the Key, V is the Value. 
+		 * Key Value pairs (Dictionary)
+		 *Values can be duplicate, but not keys
+		 *Common Implementations: HashMap, LinkedHashMap, TreeMap, Hashtable.
+		 *
+		 */
+		
+		Map<Integer, String> racerPlacements = new HashMap<Integer, String>();
+		racerPlacements.put(1, "Tommy");
+		racerPlacements.put(2, "Sally");
+		racerPlacements.put(3, "John");
+		
+		System.out.println(racerPlacements.get(1));
+		System.out.println(racerPlacements.get(3));
+		System.out.println(racerPlacements.get(2));
+		
+		
+		if (racerPlacements.containsValue("Tommy")) {
+			racerPlacements.remove(states);
+		}
+		
+		
+		Set<Integer> racerKeys = racerPlacements.keySet();
+		for (Integer key : racerKeys) {
+			System.out.println(key + " : " + racerPlacements.get(key));
+		}
+		
+		Collection<String> racers = racerPlacements.values();
+		for (String racer : racers) {
+			System.out.println(racer);
+		}
+		
+		Map<String, String> dictionary = new HashMap<String, String>();
+		dictionary.put("Augment", "make (something) greater by adding to it; increase");
+		dictionary.put("Diminish", "make or become less");
+		dictionary.put("ostentatious", "characterized by vulgar or pretentious display; designed to impress or attract notice.");
+		
+		
+		
+		
+		
+	}
+
+}
--- src/StringPractice.java
+++ src/StringPractice.java
@@ -1,11 +1,9 @@
 
-public class Practice {
+public class StringPractice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-//String Building video exercise
-		
 		String firstName = "Josh";
 		String lastName = "Smith";
 		System.out.println(firstName + lastName);
@@ -16,8 +14,17 @@ public class Practice {
 		int[] values = {5, 6, 8, 11, 17, 50};
 		System.out.println(sumArray(values));
 		
+		StringBuilder fullName = new StringBuilder("Sam");
+		fullName.append(" Smith");
+		System.out.println(fullName.toString());
 		
+		System.out.println(fullName.charAt(5));
+//		System.out.println(fullName.deleteCharAt(5));
+//		System.out.println(fullName.delete(1, 3));
 		
+		System.out.println(fullName.indexOf("it"));
+		System.out.println(fullName.replace(4, 9, "Thompson"));
+		System.out.println(fullName.reverse());
 		
 		
 		
@@ -33,23 +40,14 @@ public class Practice {
 		}
 	
 		public static String multiplyString(String str, int num) {
-			String result = "";
+			StringBuilder result = new StringBuilder();
 			for (int i = 0; i < num; i++) {
-				result += str;
+				result.append(str);
 			}
-			return result;
+			return result.toString();
 		}
 	
-		/*Write a method that takes two arrays of double and 
-		 * returns true if 
-		 * the average of the elements in the first array 
-		 * is greater than the 
-		 * average of the elements in the second array.
-		 * 
-		 */
-		
-		
-		
-		
-		
+	
+	
+
 }
